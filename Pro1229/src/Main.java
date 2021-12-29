efd36b66bc2c21a399cd6982fd57e637d8f3a5e8
Si7A-00 <Si7A-00@Si7A-06>
2021-12-29 11:00:57 +0900

    Main

---
+++ Pro1229/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int A= 49;
+		double B= 0.268300;
+		double C= A*B;
+		
+		
+		System.out.println("49 * 0.268300 = "+C);
+		System.out.printf("%d * %.6f = %.6f",A,B,C);
+	}
+
+}
